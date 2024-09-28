cbe5a2deec9994e70dac21ddfefe878486af3032
nishi-oka <nishioka31415@gmail.com>
2024-09-28 14:10:56 +0900

    Update.js

---
+++ work/js/app.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function(){
+	fetch('../data/product.json')
+		.then(response => response.json())
+		.then(products => {
+			const productTable = document.getElementById('productTable');
+			for (let i = 0; i < products.length; i++){
+				const product = products[i];
+				const createRow = document.createElement("tr");
+
+				const idCell = document.createElement("td");
+				idCell.textContent = product.id;
+        createRow.appendChild(idCell);
+				const nameCell = document.createElement("td");
+        nameCell.textContent = product.name;
+        createRow.appendChild(nameCell);
+        const priceCell = document.createElement("td");
+        priceCell.textContent = `${product.price}円`;
+        createRow.appendChild(priceCell);
+        const descriptionCell = document.createElement("td");
+        descriptionCell.textContent = product.description;
+        createRow.appendChild(descriptionCell);
+				productTable.appendChild(createRow);
+			}
+})
+.catch(error => console.error("エラー：", error));
+});
